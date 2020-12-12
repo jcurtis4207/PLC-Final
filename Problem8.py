d5a7187836fba71f13913ce30e921255802a83f6
Jacob Curtis <jcurtis4207@gmail.com>
2020-12-12 15:43:52 -0500

    upload files

---
+++ Problem8.py
@@ -0,0 +1,79 @@
+# To Do
+#
+# assignment
+# postfix unary expressions
+# 
+
+
+next_token = ''
+next_symbol = ''
+# dictionaries for symbol table
+symbol_table_value = {}
+symbol_table_type = {}
+
+def main():
+    # read in input
+    # enter all variable values and types into symbol table
+    # eg value[A] = 5, type[A] = int; value[B] = 7, type[B] = int; etc
+    EOF = False
+    while EOF == False:
+        lex()
+        if next_token == 'UNARY_OP':
+            unary_expr()
+        elif (next_token == 'VARIABLE') or (next_token == 'INT_LIT'):
+            binary_expr()
+        else:
+            error('Unrecognized Token')
+        # update EOF
+        # loop until end of file
+    # if no errors, semantics are verified
+    print('Semantic Analysis Completed With No Errors')
+    return
+
+def binary_expr():
+    operand1_type, operand2_type = ''
+    if next_token == 'VARIABLE':
+        # check symbol table for current variable
+        if not (next_symbol in symbol_table_value):
+            error('Uninitialized Variable')
+        operand1_type = symbol_table_type[next_symbol]
+    else:
+        operand1_type = 'int'
+    operand1 = next_symbol
+    lex()
+    operator = next_token
+    lex()
+    if next_token == 'VARIABLE':
+        # check symbol table for current variable
+        if not (next_symbol in symbol_table_value):
+            error('Uninitialized Variable')
+        operand2_type = symbol_table_type[next_symbol]
+    else:
+        operand2_type = 'int'
+    operand2 = next_symbol
+    # check for type mismatch
+    if symbol_table_type[operand1] != symbol_table_type[operand2]:
+        error('Type Mismatch')
+    return
+    
+def unary_expr():
+    operand_type = ''
+    operator = next_token
+    lex()
+    if next_token == 'VARIABLE':
+        # check symbol table for current variable
+        if not (next_symbol in symbol_table_value):
+            error('Uninitialized Variable')
+    return
+    
+def lex():
+    # get next_token and next_symbol from parse tree
+    return
+
+def error(message):
+    print(message)
+    # throw error and halt execution
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ RDA.java
@@ -0,0 +1,114 @@
+package Final;
+
+public class RDA {
+    public static String nextToken;
+    public static void main(String args[]){
+        // read in input
+        // get first token
+        // Start -> A { ( = | /= ) A }
+        A_expr();
+        while(nextToken == "=" || nextToken == "/="){
+            lex();
+            A_expr();
+        }
+        System.out.println("End of expression");
+    }
+    // A -> B { ( || | ~| ) B }
+    public static void A_expr(){
+        B_expr();
+        while(nextToken == "||" || nextToken == "~|"){
+            lex();
+            B_expr();
+        }
+    }
+    // B -> C { ( && | / ) C } | !C
+    public static void B_expr(){
+        if(nextToken == "!"){
+            lex();
+            C_expr();
+        }
+        else{
+            C_expr();
+            while(nextToken == "&&" || nextToken == "/"){
+                lex();
+                C_expr();
+            }
+        }
+    }
+    // C -> D { ( > | < ) D }
+    public static void C_expr(){
+        D_expr();
+        while(nextToken == ">" || nextToken == "<"){
+            lex();
+            D_expr();
+        }
+    }
+    // D -> ( - | + ) E | E { % E }
+    public static void D_expr(){
+        if(nextToken == "-" || nextToken == "+"){
+            lex();
+            E_expr();
+        }
+        else{
+            E_expr();
+            while(nextToken == "%"){
+                lex();
+                E_expr();
+            }
+        }
+    }
+    // E -> F { ( + | - | <= ) F }
+    public static void E_expr(){
+        F_expr();
+        while(nextToken == "+" || nextToken == "-" || nextToken == "<="){
+            lex();
+            F_expr();
+        }
+    }
+    // F -> G { ( * | >= | & ) G }
+    public static void F_expr(){
+        G_expr();
+        while(nextToken == "*" || nextToken == ">=" || nextToken == "&"){
+            lex();
+            G_expr();
+        }
+    }
+    // G -> { ( ++ | -- ) H } | int_lit
+    public static void G_expr(){
+        if(nextToken == "++" || nextToken == "--"){
+            lex();
+            H_expr();
+        }
+        else if(nextToken == "INT_LIT"){
+            lex();
+            // end of leaf
+        }
+        else{
+            error();
+        }
+    }
+    // H -> J { ( ++ | -- ) }
+    public static void H_expr(){
+        J_expr();
+        if(nextToken == "++" || nextToken == "--"){
+            lex();
+        }
+    }
+    // J -> variable
+    public static void J_expr(){
+        if(nextToken == "VARIABLE"){
+            lex();
+            // end of leaf
+        }
+        else{
+            error();
+        }
+    }
+    // BLANK FUNCTIONS
+    public static void lex(){
+        // parse next lexeme and set nextToken
+    }
+    public static void error(){
+        // throw error and halt execution
+    } 
+}
